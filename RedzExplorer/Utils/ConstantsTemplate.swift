95e4c3c5c2ab303a63b1615f4bdb7b66585d9e47
lqasim <lqasim@homyt.net>
2025-02-22 17:28:15 +0200

    added template for constants to use

--- RedzExplorer/Utils/ConstantsTemplate.swift
+++ RedzExplorer/Utils/ConstantsTemplate.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 
-struct Constants_Template {
+struct Constants{
     
     static var BASEURL = "<API_BASE_URL>"
     
     static var REDZ_POST_LIST_API = "\(BASEURL)/{PATH_TO_VIDEO_LIST}"
+    
+    static let categories: [String] = ["All","RECENT", "NEAREST", "FOLLOWING", "TOP_RAISING", "RECOMMENDED", "POPULAR_TRENDING"]
 }
